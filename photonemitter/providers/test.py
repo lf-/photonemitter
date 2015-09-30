c5e0a5455ba420372d8fc65363faf2c77cef0913
lf- <lf-@users.noreply.github.com>
2015-09-30 09:43:53 -0600

    Modernise test provider, add an exception test

--- photonemitter/providers/test.py
+++ photonemitter/providers/test.py
@@ -1,26 +1,45 @@
-from .. import provider
+from ..provider import Provider
 from .. import output
 
 
-class TestProvider(provider.Provider):
+class TestProvider(Provider):
     """
     A test provider that will always return the same thing, 3 lines from
     Still Alive
 
     Examples:
-    >>> tp = TestProvider('.*')
+    >>> tp = TestProvider()
     >>> tp.query('stuff')  # doctest:+ELLIPSIS
     ['This was a triumph', ..., "It's hard to overstate my satisfaction"]
     """
 
-    def __init__(self, match, **options):
-        self.match = match
+    def __init__(self, **options):
+        pass
 
     def query(self, q):
         return [output.Result(x, '') for x in ['This was a triumph',
                                                'I\'m making a note here, huge '
                                                'success', 'It\'s hard to '
                                                'overstate my satisfaction']]
+
+
+class ErrorProvider(Provider):
+    """
+    A test provider that throws an exception.
+
+    Examples:
+    >>> ep = ErrorProvider()
+    >>> ep.query('test')
+    Traceback (most recent call last):
+        ...
+    Exception: This is a test
+    """
+
+    def __init__(self, **options):
+        pass
+
+    def query(self, q):
+        raise Exception('This is a test')
 
 
 if __name__ == '__main__':
